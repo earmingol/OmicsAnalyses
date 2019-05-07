25a3faa5acbe37d321b37b54a12cf9819b1d8573
earmingol <earmingol14@gmail.com>
2019-05-07 02:09:08 -0700

    Map reads to reference genome to generate sam files

---
+++ mrnaseq/map_reads.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+module load bowtie
+
+FASTQ=/home/ucsd-train70/Erick/siob242c_data/mrnaseq/wnt_seq/fastq_files/
+OUTPUT=/home/ucsd-train70/Erick/siob242c_work/mrnaseq/outputs/
+
+export BOWTIE_INDEXES=/home/ucsd-train70/Erick/siob242c_work/reference_files/
+
+FILES="1526_CGATGT_L003_R1_001.fastq
+1526_CGATGT_L008_R1_001.fastq
+1528_TGACCA_L003_R1_001.fastq
+1528_TGACCA_L008_R1_001.fastq"
+
+
+for f in $FILES
+do
+    BASE=$(basename -- $f)
+    bowtie -v 0 -S -q hg19 $FASTQ$f $OUTPUT${BASE//.fastq/_v1.sam}
+    bowtie -v 3 -S -q hg19 $FASTQ$f $OUTPUT${BASE//.fastq/_v3.sam}
+done
