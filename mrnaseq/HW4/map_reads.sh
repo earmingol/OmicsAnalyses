ae2c0022540c768bfdd9fbf2b25a3f14ee6b537a
earmingol <earmingol14@gmail.com>
2019-05-15 15:54:37 -0700

    Homework 4 - Mapping reads and getting stats for generated sam files

---
+++ mrnaseq/HW4/map_reads.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+module load bowtie
+
+FASTQ=/home/ucsd-train70/Erick/siob242c_data/mrnaseq/wnt_seq/fastq_files/
+OUTPUT=/home/ucsd-train70/Erick/siob242c_work/mrnaseq/outputs/HW4/
+
+export BOWTIE_INDEXES=/home/ucsd-train70/Erick/siob242c_work/reference_files/
+
+# SAMPLES
+#1525	hESC Untreated 00hr
+#1526	hESC Buffer 12hr
+#1527	hESC Buffer 48hr
+#1528	hESC Wnt3a 12hr
+#1529	hESC Wnt3a 24hr
+#1530	hESC Wnt3a 48hr
+
+FILES="1526_CGATGT_L003_R1_001.fastq
+1526_CGATGT_L008_R1_001.fastq
+1528_TGACCA_L003_R1_001.fastq
+1528_TGACCA_L008_R1_001.fastq"
+
+
+for f in $FILES
+do
+    BASE=$(basename -- $f)
+    bowtie -v 3 -S --no-unal -q hg19 $FASTQ$f $OUTPUT${BASE//.fastq/.sam}
+done
+
+# Combine lines for all samples
+bowtie -v 3 -S --no-unal -q hg19 $FASTQ$"1526_CGATGT_L003_R1_001.fastq",$FASTQ$"1526_CGATGT_L008_R1_001.fastq" $OUTPUT$"1526_CGATGT_L3L8_R1_001.sam"
+
+bowtie -v 3 -S --no-unal -q hg19 $FASTQ$"1528_TGACCA_L003_R1_001.fastq",$FASTQ$"1528_TGACCA_L008_R1_001.fastq" $OUTPUT$"1528_TGACCA_L3L8_R1_001.sam"
---
+++ mrnaseq/HW4/sam_stats.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+SAM_FILES=/home/ucsd-train70/Erick/siob242c_work/mrnaseq/outputs/HW4/*.sam
+
+echo -e 'FILE\tSIZE\tTOTAL READS\tALIGNED READS\tPERCENTAGE'
+for f in $SAM_FILES
+do
+    FILENAME=$(basename -- $f)
+    SIZE=$(ls -l --block-size=GB $f| awk '{print $5}')
+    TOTAL_READS=$(tail -n +87 $f | grep -c "$")
+    ALIGNED_READS=$(tail -n +87 $f | awk '{print $3}' | grep -v "*" | grep -c "$")
+    if [ $TOTAL_READS -eq 0 ]
+    then
+        PERCENTAGE=$"0%"
+    else
+        PERCENTAGE=$((100*ALIGNED_READS/TOTAL_READS))$"%"
+    fi
+    echo -e $FILENAME$"\t"$SIZE$"\t"$TOTAL_READS$"\t"$ALIGNED_READS$"\t"$PERCENTAGE
+done
+
+
