480002898c7bb416cb270c110e5e615c3796640a
earmingol <earmingol14@gmail.com>
2019-05-18 14:20:52 -0700

    Cuffdiff time series

--- cuffdiff/cuffdiff_compare_bytimeseries.sh
+++ cuffdiff/cuffdiff_compare_bytimeseries.sh
@@ -15,24 +15,28 @@ REF=/home/ucsd-train70/Erick/siob242c_data/reference_files/cuffdiff/genes.gtf
 #1529	hESC Wnt3a 24hr
 #1530	hESC Wnt3a 48hr
 
-# L3 FILES
-L3_FILE1525=${INPUT}1525_ATCACG_L003_R1_001.sorted.dedup.bam
-L3_FILE1528=${INPUT}1528_TGACCA_L003_R1_001.sorted.dedup.bam
-L3_FILE1529=${INPUT}1529_ACAGTG_L003_R1_001.sorted.dedup.bam
-L3_FILE1530=${INPUT}1530_CAGATC_L003_R1_001.sorted.dedup.bam
-
-# L8 FILES
-L8_FILE1525=${INPUT}1525_ATCACG_L008_R1_001.sorted.dedup.bam
-L8_FILE1528=${INPUT}1528_TGACCA_L008_R1_001.sorted.dedup.bam
-L8_FILE1529=${INPUT}1529_ACAGTG_L008_R1_001.sorted.dedup.bam
-L8_FILE1530=${INPUT}1530_CAGATC_L008_R1_001.sorted.dedup.bam
-
 # Lane 3
-cuffdiff -o $OUTPUT$"TIMEL3/1528/" $REF $L3_FILE1525 $L3_FILE1528
-cuffdiff -o $OUTPUT$"TIMEL3/1529/" $REF $L3_FILE1525 $L3_FILE1529
-cuffdiff -o $OUTPUT$"TIMEL3/1530/" $REF $L3_FILE1525 $L3_FILE1530
+cuffdiff -o $OUTPUT$"TIMEL3/1528/" -L "Untreated00hr","Wnt3a12hr" $REF \
+$INPUT$"1525_ATCACG_L003_R1_001.sorted.dedup.bam" \
+$INPUT$"1528_TGACCA_L003_R1_001.sorted.dedup.bam"
+
+cuffdiff -o $OUTPUT$"TIMEL3/1529/" -L "Untreated00hr","Wnt3a24hr" $REF \
+$INPUT$"1525_ATCACG_L003_R1_001.sorted.dedup.bam" \
+$INPUT$"1529_ACAGTG_L003_R1_001.sorted.dedup.bam"
+
+cuffdiff -o $OUTPUT$"TIMEL3/1530/" -L "Untreated00hr","Wnt3a48hr" $REF \
+$INPUT$"1525_ATCACG_L003_R1_001.sorted.dedup.bam" \
+$INPUT$"1530_CAGATC_L003_R1_001.sorted.dedup.bam"
 
 # Lane 8
-cuffdiff -o $OUTPUT$"TIMEL8/1528/" $REF $L8_FILE1525 $L8_FILE1528
-cuffdiff -o $OUTPUT$"TIMEL8/1529/" $REF $L8_FILE1525 $L8_FILE1529
-cuffdiff -o $OUTPUT$"TIMEL8/1530/" $REF $L8_FILE1525 $L8_FILE1530
+cuffdiff -o $OUTPUT$"TIMEL8/1528/" -L "Untreated00hr","Wnt3a12hr" $REF \
+$INPUT$"1525_ATCACG_L008_R1_001.sorted.dedup.bam" \
+$INPUT$"1528_TGACCA_L008_R1_001.sorted.dedup.bam"
+
+cuffdiff -o $OUTPUT$"TIMEL8/1529/" -L "Untreated00hr","Wnt3a24hr" $REF \
+$INPUT$"1525_ATCACG_L008_R1_001.sorted.dedup.bam" \
+$INPUT$"1529_ACAGTG_L008_R1_001.sorted.dedup.bam"
+
+cuffdiff -o $OUTPUT$"TIMEL8/1530/" -L "Untreated00hr","Wnt3a48hr" $REF \
+$INPUT$"1525_ATCACG_L008_R1_001.sorted.dedup.bam" \
+$INPUT$"1530_CAGATC_L008_R1_001.sorted.dedup.bam"
